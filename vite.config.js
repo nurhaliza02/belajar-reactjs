c050ac1eb9be14c6fa04567a77c361a319871d5f
nurhaliza02 <nrhlza869@gmail.com>
2026-03-13 14:29:13 +0700

    add tailwindcss 2

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
+export default defineConfig(() => {
+  return {
+    plugins: [react(), tailwindcss()],
+  };
 });
